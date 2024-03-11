e8b4bcb4364e676341742a86da313195436c925d
27 Trinh Tong Hiep <tonghiep25@gmail.com>
2024-03-11 11:32:18 +0700

    Loi flick

--- 27_TrinhTongHiep_Nhanh2.cpp
+++ 27_TrinhTongHiep_Nhanh2.cpp
@@ -5,5 +5,6 @@ int main() {
 	std::cout << "Hello World!";
 	std::cout << "Hello Trinh Tong Hiep_27";
 	std::cout << "Hello Trinh Tong Hiep_27_THAY_DOI_TU_NHANH";
+	std::cout << "TAO LOI FLICK";
 	return 0;
 }
