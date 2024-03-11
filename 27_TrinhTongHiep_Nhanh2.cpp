887a35a23b7f1de9ce841b101fb5054c95a356e7
27 Trinh Tong Hiep <tonghiep25@gmail.com>
2024-03-11 11:00:55 +0700

    27 Trinh Tong Hiep Tao them file cpp trong nhanh 27-TongHiep

---
+++ 27_TrinhTongHiep_Nhanh2.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!";
+	std::cout << "Hello Trinh Tong Hiep_27";
+	std::cout << "Hello Trinh Tong Hiep_27_THAY_DOI_TU_NHANH";
+	return 0;
+}
